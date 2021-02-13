e8eeb5d29e8e53d6a1e376cf0ab414ccd685eaa2
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-13 10:42:36 -0800

    Commit Updates

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+// Require Models
+const Exercises = require('../models/Exercise')
+
+router.get('/exercises', function(req, res){
+    Exercises.find().then(exercise => res.json(exercise)).catch(err => res.status(400).json('Error: ' + err))
+})
 
 module.exports = router
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-
 // Require Models
 const Users = require('../models/Users')
 
-
 router.get('/', function(req, res) {
     Users.find().then(users => res.json(users)).catch(err => res.status(400).json('Error: ' + err))
-
     // res.send('<h1> This is the Homepage </h1>')
 })
 
