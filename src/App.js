a5af52a8dbed2cd77c1f47744eb3a83f0ca7282c
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-13 12:52:22 -0800

    Commit More

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route } from "react-router-dom"
-import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.css'
 
 // Require Components
+import Navbar from './components/Navbar'
 import ExerciseList from './components/Exercises'
 import CreateExercises from "./components/CreateExercises";
 import EditExercises from "./components/EditExercises";
+import CreateUsers from "./components/CreateUsers";
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
       <Route path="/" exact component={ExerciseList}/>
       <Route path="/create-exercise" component={CreateExercises}/>
       <Route path="/edit-exercises" component={EditExercises}/>
-      <Route path="/create-users" component={CreateExercises}/>
+      <Route path="/create-users" component={CreateUsers}/>
     </Router>
   );
 }
