2ebbda976dffbe807d42c0c8573ce214c8477c96
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-13 10:13:55 -0800

    Commit Updates

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,11 +3,13 @@ const router = express.Router()
 
 
 // Require Models
-
+const Users = require('../models/Users')
 
 
 router.get('/', function(req, res) {
-    res.send('<h1> This is the Homepage </h1>')
+    Users.find().then(users => res.json(users)).catch(err => res.status(400).json('Error: ' + err))
+
+    // res.send('<h1> This is the Homepage </h1>')
 })
 
 module.exports = router
