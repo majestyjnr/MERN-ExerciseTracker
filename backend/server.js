8db3fab0e7f6546d7f56a82c033758288a59fae5
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-13 09:02:06 -0800

    Commit Updates

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+
+
+
+
+
+// BodyParser || To GET data from form
+app.use(express.urlencoded({ extended: false }));
+
+const PORT =  process.env.PORT || 4000
+app.listen(PORT, function(){
+    console.log(`Server running on PORT ${PORT}`);
+})
