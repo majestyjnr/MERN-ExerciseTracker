de45fd27b43f3a6c614f1e66071f248887e420ac
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-13 10:10:09 -0800

    Commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,4 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 
+// Require Models
+
+
+
+router.get('/', function(req, res) {
+    res.send('<h1> This is the Homepage </h1>')
+})
+
 module.exports = router
