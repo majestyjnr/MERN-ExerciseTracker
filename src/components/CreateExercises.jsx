b8ae878e71b57f3eff82df4236ae6587b1e638bc
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-17 01:28:13 -0800

    Commit All

--- src/components/CreateExercises.jsx
+++ src/components/CreateExercises.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const CreateExercises = () => {
+
+const [username, setUsername] = useState('')
+const [description, setDescription] = useState('')
+const [duration, setDuration] = useState('')
+const [users, setUsers] = useState([])
+
+
     return (
         <div>
             <h1>Create Exercise Component</h1>
