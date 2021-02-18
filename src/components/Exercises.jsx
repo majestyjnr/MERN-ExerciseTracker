ccf3dfaafefaa77ee5fe4b719cdb1deb2aa0dce9
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-18 03:57:33 -0800

    Commit

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Exercises = () => {
+
+    const [exercises, setExercises] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:4000/exercises')
+    })
     return (
         <div>
             <h1>Exercises List Component</h1>
