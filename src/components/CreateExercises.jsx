f6176cac0a8865e8dda1a50afa50b5175c2d3649
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-18 01:25:50 -0800

    Commit All

--- src/components/CreateExercises.jsx
+++ src/components/CreateExercises.jsx
@@ -6,8 +6,9 @@ const [username, setUsername] = useState('')
 const [description, setDescription] = useState('')
 const [duration, setDuration] = useState(0)
 const [date, setDate] = useState(new Date())
-
 const [users, setUsers] = useState([])
+
+
 
 const onSubmit = (e) => {
     e.preventDefault()
@@ -19,18 +20,27 @@ const onSubmit = (e) => {
     }
     console.table(newExercise)
 
-    window.location ='/'
+    // window.location ='/'
 }
+
+
 
     return (
         <div className="container card p-5">
             <h1>Create Exercise Component</h1>
 
             <form className="add-form" onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label htmlFor="">Username:</label>
-                    <input type="text" placeholder="Username" className="form-control" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <select class="form-control" value={username} onChange={(e)=> setDate(e.target.value)} id="username">
+                        {
+                            users.map(function(user){
+                                return <option key={user} value={user}>{user}</option>
+                            })
+                        }
+                    </select>
                 </div>
+
                 <div className="form-group">
                     <label htmlFor="">Description:</label>
                     <input type="text" placeholder="Description" className="form-control" value={description} onChange={(e)=> setDescription(e.target.value)}/>
@@ -38,6 +48,10 @@ const onSubmit = (e) => {
                 <div className="form-group">
                     <label htmlFor="">Duration: (In minutes)</label>
                     <input type="number" value={duration} className="form-control" onChange={(e)=> setDuration(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">Date:</label>
+                    <input type="date" value={date} className="form-control" onChange={(e)=> setDate(e.target.value)}/>
                 </div>
 
                 <input type="submit" value="Save Task" placeholder='In Minutes' className='btn btn-primary'/>
